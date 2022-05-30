d8211bdd8624a3d26a37f145fe0b0670332412f5
Sergey Krapivnitskiy <ya.uplink@yandex.ru>
2022-05-30 19:20:42 +0300

    final

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -4,6 +4,18 @@ from django.contrib.auth.models import Group
 from .models import CustomUserCreate, Follow
 
 
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'user',
+        'following',
+    )
+    search_fields = ('user',)
+    list_filter = ('user',)
+    empty_value_display = '-пусто-'
+
+
 @admin.register(CustomUserCreate)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = (
@@ -15,18 +27,6 @@ class CustomUserAdmin(admin.ModelAdmin):
     )
     search_fields = ('email', 'username',)
     list_filter = ('email', 'username',)
-    empty_value_display = '-пусто-'
-
-
-@admin.register(Follow)
-class FollowAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'user',
-        'following',
-    )
-    search_fields = ('user',)
-    list_filter = ('user',)
     empty_value_display = '-пусто-'
 
 
